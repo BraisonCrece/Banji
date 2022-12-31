eab03fab0b95d38ebea9c92f71345fa845dfe2a9
Brais Fernández <braisoncrece@gmail.com>
2022-12-31 16:41:50 +0100

    [add] (mobile) Style product card and confeti when favorite

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,36 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+import JSConfetti from 'js-confetti'
+
+
+
+document.addEventListener("turbo:before-stream-render", function() {
+  console.log("Actualizando")
+  const confeti = document.querySelector("#confeti")
+
+  if(confeti){
+    const jsConfetti = new JSConfetti()
+    jsConfetti.addConfetti({
+      emojiSize: 50,
+      confettiNumber: 500,
+    })
+  }  
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
