446393c7ff797579910bc9b92544a3b3b45747ac
Brais Fernández <braisoncrece@gmail.com>
2023-01-03 10:43:35 +0100

    [add] (desktop) nav Styles & alert animation

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -6,4 +6,8 @@ const application = Application.start()
 application.debug = false
 window.Stimulus   = application
 
+import { Alert } from "tailwindcss-stimulus-components"
+
+application.register('alert', Alert)
+
 export { application }
--- config/importmap.rb
+++ config/importmap.rb
@@ -2,7 +2,8 @@
 
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "js-confetti", to: "https://ga.jspm.io/npm:js-confetti@0.11.0/dist/es/index.js"
+pin "tailwindcss-stimulus-components" # @3.0.4
+pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.1
