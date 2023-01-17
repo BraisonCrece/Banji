5c4e1d63581520e449a586122ccf6435049b041a
BraisonCrece <braisoncrece@gmail.com>
2023-01-17 12:02:18 +0100

    [fix] Delete favorite = delete all :S

--- app/javascript/controllers/gallery_controller.js
+++ app/javascript/controllers/gallery_controller.js
@@ -2,20 +2,46 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="gallery"
 export default class extends Controller {
-  static targets = ["images_gallery", "point"]
+  static targets = ["images", "points", "imagesGallery"]
+
   connect() {
-    this.pointTargets.forEach((point, index) => {
-      point.addEventListener("click", () => {
-        let position = index
-        let operation = position * (100 / (this.pointTargets.length))
+    // depending on the scroll position of the images gallery, the corresponding point will be highlighted
+    // Obtener el enlace
+    let links = this.pointsTargets
+    this.pointsTargets[0].classList.add("bg-blue-500")
+    let images = this.imagesTargets;
 
-        this.images_galleryTarget.style.transform = `translateX(${operation}%)`
 
-        this.pointTargets.forEach((point) => {
-          point.classList.remove("bg-blue-500")
-        })
-        point.classList.add("bg-blue-500")
-      })
+    // Recorrer todos los enlaces
+    links.forEach((link) => {
+      // Agregar un evento click a cada enlace
+      link.addEventListener("click", (event) => {
+        // Prevenir el comportamiento por defecto
+        event.preventDefault();
+        // Obtener el elemento al cual apunta el enlace
+        let elemento = document.querySelector(link.getAttribute("href"));
+        // Desplazarse suavemente hacia el elemento
+        elemento.scrollIntoView({ behavior: "smooth" });
+
+        // remove the bg-blue-500 class from all the points
+        links.forEach((link) => {
+          link.classList.remove("bg-blue-500")
+        });
+        event.target.classList.add("bg-blue-500")
+      });
+
     })
+
+
+
   }
+
+
+
+
+
+
+
+
+
 }
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,6 +1,6 @@
 class Favorite < ApplicationRecord
   validates :user, uniqueness: { scope: :product }
 
-  belongs_to :user, dependent: :destroy
-  belongs_to :product, dependent: :destroy
+  belongs_to :user
+  belongs_to :product
 end
