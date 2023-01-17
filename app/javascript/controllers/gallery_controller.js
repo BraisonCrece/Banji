5910e305bf6407321ee4f5d355f4e42747feb849
BraisonCrece <braisoncrece@gmail.com>
2023-01-17 12:53:04 +0100

    [add] show product styles

--- app/javascript/controllers/gallery_controller.js
+++ app/javascript/controllers/gallery_controller.js
@@ -5,28 +5,20 @@ export default class extends Controller {
   static targets = ["images", "points", "imagesGallery"]
 
   connect() {
-    // depending on the scroll position of the images gallery, the corresponding point will be highlighted
-    // Obtener el enlace
     let links = this.pointsTargets
-    this.pointsTargets[0].classList.add("bg-blue-500")
     let images = this.imagesTargets;
+    this.pointsTargets[0].classList.add("bg-blue-500")
 
-
-    // Recorrer todos los enlaces
     links.forEach((link) => {
-      // Agregar un evento click a cada enlace
       link.addEventListener("click", (event) => {
-        // Prevenir el comportamiento por defecto
         event.preventDefault();
-        // Obtener el elemento al cual apunta el enlace
         let elemento = document.querySelector(link.getAttribute("href"));
-        // Desplazarse suavemente hacia el elemento
         elemento.scrollIntoView({ behavior: "smooth" });
 
-        // remove the bg-blue-500 class from all the points
         links.forEach((link) => {
           link.classList.remove("bg-blue-500")
         });
+
         event.target.classList.add("bg-blue-500")
       });
 
@@ -35,13 +27,5 @@ export default class extends Controller {
 
 
   }
-
-
-
-
-
-
-
-
 
 }
